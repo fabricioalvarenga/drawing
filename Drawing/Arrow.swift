7b928e8c9443b868e92454edf82d43fdbda6f631
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2022-08-14 09:24:25 -0300

    Initial commit

---
+++ Drawing/Arrow.swift
@@ -0,0 +1,52 @@
+//
+//  Arrow.swift
+//  Drawing
+//
+//  Created by FABRICIO ALVARENGA on 14/08/22.
+//
+
+import SwiftUI
+
+struct ArrowShape: Shape {
+    var width: CGFloat
+    let height: CGFloat = 200
+    
+    init(width: CGFloat) {
+        self.width = width
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.midY / 3))
+        path.addLine(to: CGPoint(x: rect.midX / 2, y: rect.midY / 1.5))
+        path.addLine(to: CGPoint(x: (rect.maxX - rect.midX / 2), y: rect.midY / 1.5))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.midY / 3))
+        path.closeSubpath()
+
+        path.addRect(CGRect(x: rect.midX - (width / 2), y: rect.midY / 1.5, width: width, height: height))
+        
+        return path
+    }
+    
+    
+}
+
+struct Arrow: View {
+    @State private var width: CGFloat = 30
+    
+    var body: some View {
+        VStack {
+            ArrowShape(width: width)
+                .stroke(lineWidth: width)
+            
+            Slider(value: $width.animation(.linear(duration: 1)), in: 1...50)
+        }
+    }
+}
+
+struct Arrow_Previews: PreviewProvider {
+    static var previews: some View {
+        Arrow()
+    }
+}
---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by FABRICIO ALVARENGA on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
